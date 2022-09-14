9fb3acc92206ab9215f05b13ccb089b309d85bfe
AmirAliEidivandi <amirah648887@gmail.com>
2022-09-14 21:58:26 +0430

    added objects

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import Head from "next/head";
 
+const posts = [
+    { title: "React Testing", excerpt: "Learn React Testing" },
+    { title: "React With Tailwind", excerpt: "Learn React With Tailwind" },
+];
+
 export default function Home() {
     return (
-        <div className="text-center">
+        <div className="container mx-auto px-10 mb-8 bg-gray-300">
             <Head>
-                <title>Blog</title>
+                <title>CMS Blog</title>
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
             </Head>
 
-            <h1>hello world</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="lg:col-span-8 col-span-1">
+                    {posts.map((post, index) => (
+                        <>
+                            <div>
+                                {post.title}
+                                {post.excerpt}
+                            </div>
+                        </>
+                    ))}
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8"></div>
+                </div>
+            </div>
         </div>
     );
 }
